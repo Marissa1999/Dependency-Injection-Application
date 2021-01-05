78480103f2fbf9c0c1a5de0c3421f3a25328bd35
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-04 19:36:19 -0500

    Initial Commit.

---
+++ MarissaGoncalvesLab7/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarissaGoncalvesLab7
+{
+    public class HomeController
+    {
+
+        private readonly IGreetingService _greetingService;
+
+        public HomeController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService ?? throw new ArgumentNullException(nameof(greetingService));
+        }
+
+        public string Hello(string name) => _greetingService.Greet(name);
+
+    }
+}
---
+++ MarissaGoncalvesLab7/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarissaGoncalvesLab7
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceProvider container = RegisterServices())
+            {
+                var controller = container.GetRequiredService<HomeController>();
+                string result = controller.Hello("VANIER");
+                Console.WriteLine(result);
+            }
+        }
+
+        static ServiceProvider RegisterServices()
+        { 
+            var services = new ServiceCollection();
+            services.AddSingleton<IGreetingService, GreetingService>();
+            services.AddTransient<HomeController>();
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
